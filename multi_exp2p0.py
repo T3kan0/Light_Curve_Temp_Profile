e557f56c1e972a7c7558ec2a4a447fdeb6b1824d
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2023-08-24 11:07:09 +0200

    Add files via upload

--- multi_exp2p0.py
+++ multi_exp2p0.py
@@ -6,14 +6,19 @@ Created on Fri Mar  6 14:34:59 2020
 @author: tekano
 """
 
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.optimize import curve_fit
+
+#Import python library
+
+import matplotlib.pyplot as plt # for visuals
+import numpy as np # data preparation
+from scipy.optimize import curve_fit # fitting library
 #matplotlib.use('Qt5Agg')
+
+
+#Upload the data
 
 gamma = np.genfromtxt('3C279_2017_phaseII_12h_LC.asc', 
                       names = True,
-#                      skip_header=12,
                       autostrip=True, 
                       delimiter = '')
 
@@ -21,8 +26,10 @@ HE_Phot = np.genfromtxt('HE_0.75deg_evclass512_10GeV_gtsrcprob_LP_P8_UFS.txt',
                         names = True, 
                         autostrip=True, 
                         delimiter = '')
+
+# Functions for the individual exponential peaks/profiles, as well as for combining them to get the aggregated fit.
     
-def peak(t, t0, F0, Tr, Tf):
+def peak(t, t0, F0, Tr, Tf): # Individual expo profiles
     
 
     exp = (2 * F0 / 
@@ -31,7 +38,7 @@ def peak(t, t0, F0, Tr, Tf):
     return exp
 
 
-def combine_peaks(t, *params):
+def combine_peaks(t, *params):  # Combining profles
     params_nd = np.array(params).flatten()
 #    print(params_nd)
     p_par = params_nd[1:].reshape(-1, 4)
@@ -42,23 +49,25 @@ def combine_peaks(t, *params):
         lc += peak(t, i[0], i[1], i[2], i[3])
     
     return lc
-        
+
+# Data preparation
+
 gamma = np.sort(gamma, order = 'MID_MJD')
 mask = (gamma['MID_MJD'] >= 57823.75) & (gamma['MID_MJD'] <= 57855.25)
 gammadat = gamma[mask]
 
-params = np.array([#[ 57820.88, 0.5e-06, 0.5, 0.8],
-                   #[57826.625, 3.1e-06, 0.8, 0.5],
-                  [57829.00, 1.0e-06, 1.0, 1.0],
+# User input params: These are the initial/assumed parameters for the model.
+params = np.array([57829.00, 1.0e-06, 1.0, 1.0],
                   [57834.10, 2.10e-06, 1.0, 1.10],
                   [57844.50, 4.50e-06, 0.4, 0.7],
                   [57845.50, 3.5e-06, 0.6, 0.1],
                   [57849.00, 4.2e-06, 0.6, 0.7],
                   [57851.10, 2.9e-06, 0.4, 0.7]
-                  # [57197.10, 2.50e-06, 0.3, 0.3]
                    ])
 params_f = params.flatten()
 params_f = np.concatenate([[0.40e-6], params_f])
+
+# Data Visualization
 
 fig, (ax0, ax1) = plt.subplots(2, 1,
      sharex=True,
@@ -141,6 +150,7 @@ ax1.set_ylabel('Photon Energy (GeV)',
                fontsize= 14)
 ax1.tick_params(axis='both', which='major', labelsize=13)
 
+# Fitting and Optimization of the model.
 
 popt, pcov = curve_fit(combine_peaks,
                        gammadat['MID_MJD'],
@@ -155,25 +165,25 @@ new_mjd = np.linspace(gammadat['MID_MJD'][0],
                       10000)
 
 fit = combine_peaks(new_mjd, 
-                    popt)
+                    popt) # Fitting the curves
 
 chisq = sum(((gammadat['Flux'] 
               - combine_peaks(gammadat['MID_MJD'], 
-                              popt)) / gammadat['FluxErr'])**2)
+                              popt)) / gammadat['FluxErr'])**2) # Chi-square of the fit.
 
-ndf = len(gammadat['Flux']) - (4*6 + 1)
-norm_chisq = chisq / ndf
+ndf = len(gammadat['Flux']) - (4*6 + 1) 
+norm_chisq = chisq / ndf # Normalized Chi-squared.
 
 a = popt[1:].reshape(-1, 4)
 
 dbl_time = np.log(2)*24*a[:,2]
 
-R = 1.079e+14*30.0*dbl_time / (1 + 0.536)
-
 tps = a[:,0] + (a[:,2]*a[:,3] / (a[:,2] + a[:,3])
                 ) * np.log(a[:,3] / a[:,2])
 
 skw_param = (a[:,3] - a[:,2])/(a[:,3] + a[:,2])  
+
+# A look at the optimized parameters.
 
 print('baseline Flux', popt[0], '\n', 'baseline FluxErr', np.sqrt(np.diag(pcov))[0])
 print('optimized params', '\n', popt[1:].reshape(-1, 4))
@@ -181,13 +191,10 @@ print('optimized params error', '\n', np.sqrt(np.diag(pcov))[1:].reshape(-1, 4))
 print('Skewness Parameters', '\n', skw_param)
 print('doublng times', '\n' , dbl_time.reshape(1, 6))
 print('peak_MJDs', '\n', tps)
-print('emission-region sizes', '\n', R)
 print('chi-square:', chisq, '\n', 'ndf', ndf)
 print('reduced chisq:', norm_chisq)
 
-
-#fit_butterfly = combine_peaks(gamma['MID_MJD'], popt + err)
-#fit_butterfly_ = combine_peaks(gamma['MID_MJD'], popt - err)
+# Model Visualization
 
 ax0.plot(new_mjd, fit, linestyle = 'solid', linewidth=2.0)
 #plt.figtext(0.62, 0.82, 'Norm Chisq: %.2f'%norm_chisq, fontweight = 'bold')
@@ -195,24 +202,8 @@ for idx, i in enumerate(popt[1:].reshape(-1, 4)):
     test_2 = peak(new_mjd, i[0], i[1], i[2], i[3])
     ax0.plot(new_mjd, test_2, linestyle = 'solid', linewidth=0.5, label=('peak' + str(idx+1)))
 
-
-#residuals = gammadat['Flux'] - #combine_peaks(gammadat['MID_MJD'], popt)
-#ax1.errorbar(gammadat['MID_MJD'],
-#                y=residuals,
-#                 yerr=gammadat['FluxErr'],
-##                 uplims=uplim,
-#                 fmt='o', 
-#                 c='black',
-#                 ms=1.0,
-#                 label = 'LAT')
-#ax1.ticklabel_format(style='sci', axis = 'y', scilimits = (0,0),
-#                         useMathText=True)
-#ax1.set_ylabel('Residual (data - model)')
 ax1.set_xlabel('Time (MJD)', fontsize = 14)
-#ax1.hlines(0, 57822.75, 57855.00, linestyle = 'solid')
-#ax0.legend()
 plt.show()
-#plt.savefig('3C279_12hr_PhaseII_tempX_E.eps')
 
 
 
